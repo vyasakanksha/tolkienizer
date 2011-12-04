af8ceda9aa3a79f4a45fca96e4996a7ebb6dc48d
Akanksha Vyas <vyasakanksha@gmail.com>
2011-12-04 12:33:07 -0500

    Code for int and string tokens is complete, tested and commented.
    Tested the code for stringToken and finished commenting it. I
    might find more errors when I actually start using it.

--- src/lexer/token.go
+++ src/lexer/token.go
@@ -1,20 +1,31 @@
-package lexer
+package main
 
-//import "fmt"
+/* import "fmt" */
 
+// An interface for Token that can be of any type as long it implements 
+// Cmp (compare). 
 type Token interface {
    Cmp( t Token ) bool
 }
 
+// Two current implementions of Token are for ints and string. They are declared
+// here.
 type intToken int
 type stringToken string
 
 // Functions for intToken 
+
+// Default constructor - takes an integer as input, converts it into a intToken
+//and returns a pointer to the intToken
 func NewIntToken( n int ) * intToken {
    i := intToken( n )
    return &i
 }
 
+// This function takes a token as input and compares it to an intToken,
+// returning true if they are equal. It first checks if the input token is 
+// indeed an intToken using type reflection.
+// The func returns false on incompatable types and inequality
 func ( this * intToken ) Cmp( t Token ) bool {
    if i, ok := t.(*intToken); ok {
       return *this == *i
@@ -26,6 +37,18 @@ func ( this * intToken ) Cmp( t Token ) bool {
 }
 
 // Functions for stringToken 
+
+// Default constructor - takes an String as input, converts it into a
+// stringToken and returns a pointer to the stringToken.
+func NewStringToken( s string ) * stringToken {
+   i := stringToken( s )
+   return &i
+}
+
+// This function takes a token as input and compares it to a stringToken,
+// returning true if they are equal. It first checks if the input token is 
+// indeed an stringToken using type reflection.
+// The func returns false on incompatable types and inequality
 func ( this * stringToken ) Cmp( t Token ) bool {
    if i, ok := t.(*stringToken); ok {
       return *this == *i
@@ -36,15 +59,10 @@ func ( this * stringToken ) Cmp( t Token ) bool {
    panic( "Should never get here!" )
 }
 
-func NewToken( s string ) * stringToken {
-   i := stringToken( s )
-   return &i
-}
-
 /*func main() {
-   i := NewIntToken( 5 )
-   j := NewIntToken( 5 )
-   k := NewIntToken( 6 )
+   i := NewStringToken( "thanks for all" )
+   j := NewStringToken( "thanks for all" )
+   k := NewStringToken( "fish" )
 
    fmt.Println( i.Cmp( j ) )
    fmt.Println( i.Cmp( k ) )
