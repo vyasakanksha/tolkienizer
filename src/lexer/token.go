03d9ff890045407f4a6236726fd30dd3c05a3599
Akanksha Vyas <vyasakanksha@gmail.com>
2011-12-03 21:43:13 -0500

    Int and String tokens implemented.
    I implemented intToken and stringToken, and their associated
    functions. IntToken has been tested but string token has not.

--- src/lexer/token.go
+++ src/lexer/token.go
@@ -1,35 +1,51 @@
 package lexer
 
-import(
-   "os"
-   "fmt"
-   )
+//import "fmt"
 
-type Token struct {
-   iSet *int
-   sSet *string
+type Token interface {
+   Cmp( t Token ) bool
 }
 
-func Cmp( a Token, b Token ) bool {
-   switch {
-      case a.iSet == nil && b.iSet == nil:
-            if a.sSet == nil || b.sSet == nil {
-               fmt.Fprintf( os.Stderr, "Invalid Input" )
-            } else {
-               if &a == &b {
-                  return true
-               } else { return false }
-            }
-      case a.sSet == nil && b.sSet == nil:
-            if a.iSet == nil || b.iSet == nil {
-               fmt.Fprintf( os.Stderr, "Invalid Input" )
-            } else {
-               if &a == &b {
-                  return true
-               } else { return false }
-            }
+type intToken int
+type stringToken string
+
+// Functions for intToken 
+func NewIntToken( n int ) * intToken {
+   i := intToken( n )
+   return &i
+}
+
+func ( this * intToken ) Cmp( t Token ) bool {
+   if i, ok := t.(*intToken); ok {
+      return *this == *i
+   } else {
+      return false
    }
 
-  fmt.Fprintf( os.Stderr, "Inconsistent Types" )
-  return false
+   panic( "Should never get here!" )
 }
+
+// Functions for stringToken 
+func ( this * stringToken ) Cmp( t Token ) bool {
+   if i, ok := t.(*stringToken); ok {
+      return *this == *i
+   } else {
+      return false
+   }
+
+   panic( "Should never get here!" )
+}
+
+func NewToken( s string ) * stringToken {
+   i := stringToken( s )
+   return &i
+}
+
+/*func main() {
+   i := NewIntToken( 5 )
+   j := NewIntToken( 5 )
+   k := NewIntToken( 6 )
+
+   fmt.Println( i.Cmp( j ) )
+   fmt.Println( i.Cmp( k ) )
+}*/
