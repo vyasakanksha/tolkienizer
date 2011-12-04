067c672492159ed37f9a4d980ae83d0e4dc9ef6d
Akanksha Vyas <vyasakanksha@gmail.com>
2011-12-03 21:16:39 -0500

    Added Prefix() and Next() for NReaderBase. Not tested.
    I fixed the prefix function so that is accounts for n being larger
    than the length of tokenSet. I still have not tested it. Should
    get on that. I also implemented Next() which was simple and
    should work fine.

--- src/lexer/nreader.go
+++ src/lexer/nreader.go
@@ -1,32 +1,59 @@
 package lexer
 
+import (
+   "bufio"
+)
+
 type NReader interface {
 
-   // returns the last n tokens for the current Token
+   // This function returns the last n tokens in the tokenSet
    Prefix() []Token
 
-   // returns the next Token for a slice of n tokens
-   Next Token()
+   // This function returns the most recent token in the tokenSet
+   Next() Token
 
-   // Reads a Token from the reader and appends it to the tokenSet
+   // This function reads a new token and adds it to tokenSet
    Advance()
 }
 
-type NReaderBase {
+type NReaderBase struct {
    n int
    next Token
    tokenSet []Token
-   r Reader
+   r bufio.Reader
+   Token this.negOne
 }
 
-func ( NReaderBase ) Prefix() []Token {
+
+// This function looks at the righmost value in the tokenset and returns a slice
+// containing the last n tokens
+func ( this NReaderBase ) Prefix() []Token {
    j := 0
-   length := len( tokenSet )
-   prefixSet := make( []Token, length - n )
-   for i := ( length - n ); i < length; i++ {
-      prefixSet[j] = tokenSet[i]
+   length := len( this.tokenSet )
+   prefixSet := make( []Token, this.n )
+
+   if length < this.n {
+      for j := 0; j < (this.n - length); j++ {
+         prefixSet[j] = this.negOne;
+      }
+   }
+
+   for i := ( length - this.n ); i < length; i++ {
+      prefixSet[j] = this.tokenSet[i]
       j++
    }
+   return prefixSet
+}
+
+//This function reads a new value, converts it into a token and adds it to
+//tokenSet
+func ( this NReaderBase ) Advance() {
+  temp, _, err := this.r.ReadRune()
+}
+
+// This function returns the last token from tokenSet
+func ( this NReaderBase ) Next() {
+   return this.tokenSet[ len( tokenSet ) - 1 ]
 }
 
 //type NReaderSpace struct {
