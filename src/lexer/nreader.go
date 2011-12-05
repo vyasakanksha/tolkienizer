0ddd474b2ee934383440f45041aeb85db9518990
Akanksha Vyas <vyasakanksha@gmail.com>
2011-12-05 03:04:06 -0500

    Implemented Advance() - not commented or tested
    Implemented Advance() for the three different readers. Modified
    the function so that successful reads return true, else false.

--- src/lexer/nreader.go
+++ src/lexer/nreader.go
@@ -13,7 +13,7 @@ type NReader interface {
    Next() string
 
    // This function reads a new string and adds it to tokenSet
-   Advance()
+   Advance() bool
 }
 
 // A base struct off which each reader extends
@@ -31,31 +31,75 @@ type nReaderBase struct {
    r  bufio.Reader
 }
 
-type nReaderSpace struct {
+// Type that treats every letter (including punctuation and space) as a token
+type nReaderLetter struct {
    nReaderBase
 }
 
-type nReaderNoSpace struct {
+// Type that treats every letter as a token, and treats every word as
+// a different problem. I.e. everytime it encounters a space or punctuation, 
+// it stops reading.
+type nReaderLetterDelim struct {
    nReaderBase
 }
 
+// Type that treats each word as a token
 type nReaderWords struct {
    nReaderBase
 }
 
-// This function looks at the righmost value in the tokenset and returns a slice
-// containing the last n tokens
-func (this NReaderBase) Prefix() []string {
+func (this nReaderLetterDelim) Advance() bool {
+   temp, _, err := this.r.ReadRune()
+   if err != nil || temp == '.' {
+      return false
+   } else {
+      this.stringSet[ len(this.stringSet) ] = string(temp)
+      return true
+   }
+
+   panic( "We should never get here")
+}
+
+
+func (this nReaderLetter) Advance() bool {
+   temp, _, err := this.r.ReadRune()
+   if err != nil {
+      return false
+   } else {
+      this.stringSet[len(this.stringSet)] = string(temp)
+      return true
+   }
+
+   panic( "We should never get here")
+}
+
+func (this nReaderWords) Advance() bool {
+   temp, err := this.r.ReadString('.')
+   if err != nil {
+      return false
+   } else {
+      this.stringSet[len(this.stringSet)] = temp
+      return true
+   }
+
+   panic( "We should never get here")
+}
+
+// Return: Last n values of the stringSet (excuding the current value)
+func (this nReaderBase) Prefix() []string {
    j := 0
    length := len(this.stringSet)
    prefixSet := make([]string, this.n)
 
+   // If n is larger than the stringSet, initialize prefixSet[:n-length] to
+   // the empty string
    if length < this.n {
       for j := 0; j < (this.n - length); j++ {
-         prefixSet[j] = "-1"
+         prefixSet[j] = ""
       }
    }
 
+   // Copy the last n (excluding length) tokens to prefix
    for i := (length - this.n); i < length; i++ {
       prefixSet[j] = this.stringSet[i]
       j++
@@ -63,13 +107,8 @@ func (this NReaderBase) Prefix() []string {
    return prefixSet
 }
 
-// This function returns the last string from stringSet
-func (this NReaderBase) Next() string {
+// This function returns the current string from stringSet
+func (this nReaderBase) Next() string {
    return this.stringSet[len(this.stringSet)-1]
 }
 
-//This function reads a new value, converts it into a string and adds it to
-//stringSet
-func (this NReaderSpace) Advance() {
-   temp, _, err := this.r.ReadRune()
-}
