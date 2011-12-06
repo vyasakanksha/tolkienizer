8246e2b412ce50dbe2083b564776f2ebd13d69ef
Sean Anderson <fnordit@Galactica.Earth>
2011-12-06 04:59:21 -0500

    Formatted

--- src/lexer/delimreader.go
+++ src/lexer/delimreader.go
@@ -5,7 +5,6 @@ import (
    "io"
    "bufio"
    "utf8"
-   "fmt"
 )
 
 type DelimReader struct {
@@ -22,41 +21,40 @@ func NewDelimReader(read io.Reader, delim []int, used int) io.Reader {
 func (r *DelimReader) Read(p []byte) (n int, err os.Error) {
    bytes_written := 0
 
-<<<<<<< HEAD
-    if (r.remainder != nil) {
-        for i := 0; i < len(r.remainder); i++ {
-            p[i] = r.remainder[i]
-            bytes_written++
-        }
-        r.remainder = nil
-    }
+   if r.remainder != nil {
+      for i := 0; i < len(r.remainder); i++ {
+         p[i] = r.remainder[i]
+         bytes_written++
+      }
+      r.remainder = nil
+   }
 
-    for (bytes_written < len(p)) {
-        rune, size, err := r.reader.ReadRune()
-        if (err != nil) {
-            return bytes_written, err
-        }
-        for _, value := range r.delimiters {
-            if (value == rune) {
-                rune = r.used_delimiter
-                size = utf8.RuneLen(rune)
-            }
-        }
-        if (bytes_written + size > len(p)) {
-            // we need to split the rune and hold on to the remainder
-            writable := len(p) - bytes_written
-            target := make([]byte, size)
-            _ = utf8.EncodeRune(target, rune)
-            for i := 0; i < writable; i++ {
-                p[bytes_written] = target[i]
-                bytes_written++
-            }
-            r.remainder = target[writable:]
-        } else {
-            target := p[bytes_written:bytes_written + size]
-            _ = utf8.EncodeRune(target, rune)
-            bytes_written += size
-        }
-    }
-    return bytes_written, nil
+   for bytes_written < len(p) {
+      rune, size, err := r.reader.ReadRune()
+      if err != nil {
+         return bytes_written, err
+      }
+      for _, value := range r.delimiters {
+         if value == rune {
+            rune = r.used_delimiter
+            size = utf8.RuneLen(rune)
+         }
+      }
+      if bytes_written+size > len(p) {
+         // we need to split the rune and hold on to the remainder
+         writable := len(p) - bytes_written
+         target := make([]byte, size)
+         _ = utf8.EncodeRune(target, rune)
+         for i := 0; i < writable; i++ {
+            p[bytes_written] = target[i]
+            bytes_written++
+         }
+         r.remainder = target[writable:]
+      } else {
+         target := p[bytes_written : bytes_written+size]
+         _ = utf8.EncodeRune(target, rune)
+         bytes_written += size
+      }
+   }
+   return bytes_written, nil
 }
--- src/lexer/delimreader_test.go
+++ src/lexer/delimreader_test.go
@@ -6,45 +6,44 @@ import (
 )
 
 func TestThatThang(t *testing.T) {
-<<<<<<< HEAD
-    delim := make([]int, 4)
-    delim[0] = ' '
-    delim[1] = ','
-    delim[2] = '.'
-    delim[3] = 'þ'
-    used := '.'
-    f, _ := os.Open("samplefile")
-    defer f.Close()
-    dr := NewDelimReader(f, delim, used)
+   delim := make([]int, 4)
+   delim[0] = ' '
+   delim[1] = ','
+   delim[2] = '.'
+   delim[3] = 'þ'
+   used := '.'
+   f, _ := os.Open("samplefile")
+   defer f.Close()
+   dr := NewDelimReader(f, delim, used)
 
-    atestslice := make([]byte, 4)
-    num, err := dr.Read(atestslice)
-    if (num != 4 || err != nil) {
-        t.Fail()
-    }
-    if (string(atestslice) != "Mini") {
-        t.Fail()
-    }
-    num, err = dr.Read(atestslice)
-    if (num != 4 || err != nil || string(atestslice) != "m.lo") {
-        t.Fail()
-    }
-    num, err = dr.Read(atestslice)
-    if (num != 4 || err != nil || string(atestslice) != "ಠv") {
-        t.Fail()
-    }
-    num, err = dr.Read(atestslice)
-    if (num != 4 || err != nil) {
-        t.Fail()
-    }
-    if (atestslice[0] != 'e' || atestslice[1] != 'l' || atestslice[2] != 224 || atestslice[3] != 178) {
-        t.Fail()
-    }
-    num, err = dr.Read(atestslice)
-    if (num != 4 || err != nil) {
-        t.Fail()
-    }
-    if (atestslice[0] != 160 || string(atestslice[1:]) != "it.") {
-        t.Fail()
-    }
+   atestslice := make([]byte, 4)
+   num, err := dr.Read(atestslice)
+   if num != 4 || err != nil {
+      t.Fail()
+   }
+   if string(atestslice) != "Mini" {
+      t.Fail()
+   }
+   num, err = dr.Read(atestslice)
+   if num != 4 || err != nil || string(atestslice) != "m.lo" {
+      t.Fail()
+   }
+   num, err = dr.Read(atestslice)
+   if num != 4 || err != nil || string(atestslice) != "ಠv" {
+      t.Fail()
+   }
+   num, err = dr.Read(atestslice)
+   if num != 4 || err != nil {
+      t.Fail()
+   }
+   if atestslice[0] != 'e' || atestslice[1] != 'l' || atestslice[2] != 224 || atestslice[3] != 178 {
+      t.Fail()
+   }
+   num, err = dr.Read(atestslice)
+   if num != 4 || err != nil {
+      t.Fail()
+   }
+   if atestslice[0] != 160 || string(atestslice[1:]) != "it." {
+      t.Fail()
+   }
 }
