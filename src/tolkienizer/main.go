e940ff4cc9b2ef4ee1a0f18aa3bd9d1bc04b517a
Samuel C. Payson <scpayson@gmail.com>
2011-12-08 09:24:55 -0500

    Working!

--- src/tolkienizer/main.go
+++ src/tolkienizer/main.go
@@ -1,8 +1,22 @@
 package main
 
-import "fmt"
-import "lexer"
+import (
+   "fmt"
+   "lexer"
+   "pfa"
+   "os"
+)
 
 func main() {
    fmt.Printf("Tolkienizer v0.0\n")
+   nr := lexer.NewNReaderLetterDelim(3, os.Stdin, []int{' ', '.', ',', ';',
+      '\n', '\'', '"', '`', ':', '-', '\t'})
+
+   tpfa := pfa.NewTablePFA(nr)
+
+   tpfa.Learn()
+
+   for i := 0; i < 20; i++ {
+      fmt.Println(tpfa.GenerateString())
+   }
 }
