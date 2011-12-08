90fae4bc488180c230aef7681be7e38cc614888c
Samuel C. Payson <scpayson@gmail.com>
2011-12-08 01:46:14 -0500

    Fixed code in NReader.
    Refactored 'Prefix()' and 'Next()' to be more elegant and changed
    all nReader* methods to have pointer recievers.

--- src/lexer/nreader_test.go
+++ src/lexer/nreader_test.go
@@ -5,24 +5,24 @@ import (
    "strings"
 )
 
-func nReadersTest(t *testing.T) {
+func TestNReaders(t *testing.T) {
    // Test 1: nReaderLetter
    r := strings.NewReader("h ah")
-   temp := NewNReaderLetter( 3, r )
+   temp := NewNReaderLetter(3, r)
 
    if temp.Advance() != false {
 
-      test := []string{"","","h"}
+      test := []string{"", "", "h"}
 
-      for i:= 0; i < len(test); i++  {
-         if temp.Prefix()[i] != test[i] {
-         t.Errorf("Expected \"h\" != %s", temp.Prefix() )
+      for i, rune := range temp.Prefix() {
+         if rune != test[i] {
+            t.Errorf("Expecting \"%#v\", got \"%#v\"", test, temp.Prefix())
          }
       }
    }
 }
 
-  /* delim := {' '}
+/* delim := {' '}
    newNReaderLettersDelim(3, r, ' ')
    Advance()
 
